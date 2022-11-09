40c3d3883500288fe150b95def1822d9497afa1f
Long Vo <long.vo@pops.vn>
2022-11-09 18:36:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import Firefly from 'firefly-react';
-import Heart from './heart';
+import Lottie from 'react-lottie';
+import * as animationData from './73482-heart.json';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
 
 const colors = ['Yellow', 'Orange'];
 
 const App = () => {
-  const [isClick, setClick] = useState(false);
-
   const [canvasHeight, setCanvasHeight] = useState(window.innerHeight);
   const [canvasWidth, setCanvasWidth] = useState(window.innerWidth);
-
+  const heartSize = canvasWidth < canvasHeight ? canvasWidth / 2 : canvasHeight / 2;
   window.addEventListener(
     'resize',
     (e) => {
@@ -20,14 +28,12 @@ const App = () => {
     false
   );
 
-  const onClick = useCallback(() => {
-    setClick(!isClick);
-  }, [isClick]);
-
   return (
     <div className="App">
       <header className="App-header">
-        <Heart isClick={isClick} onClick={onClick} />
+        <div style={{ position: 'absolute' }}>
+          <Lottie options={defaultOptions} height={heartSize} width={heartSize} />
+        </div>
         <Firefly canvasWidth={canvasWidth} canvasHeight={canvasHeight} colors={colors} />
       </header>
     </div>
